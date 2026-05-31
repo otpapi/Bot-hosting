5f64ec720d78bb56b4493c0fea5893be70edee98
otpapi <otp.sender.api@gmail.com>
2026-05-31 21:57:25 +0530

    Update welcome.js

--- events/welcome.js
+++ events/welcome.js
@@ -2,26 +2,36 @@ const { Markup } = require("telegraf");
 
 module.exports = (bot) => {
 
-  // JOIN MESSAGE
+  // MEMBER JOIN
   bot.on("new_chat_members", async (ctx) => {
 
     const members = ctx.message.new_chat_members;
 
     for (const user of members) {
 
-      const username =
-        user.username
-          ? "@" + user.username
-          : "NOT SET";
+      const fullName = [
+        user.first_name,
+        user.last_name
+      ]
+        .filter(Boolean)
+        .join(" ");
+
+      const username = user.username
+        ? "@" + user.username
+        : "NOT SET";
+
+      const mention = `<a href="tg://user?id=${user.id}">${fullName}</a>`;
 
       const text = `
-🌟 **WELCOME** @${user.first_name}!
+🎸☠️ ✧Supreme✧ ☠️🎸
 
-📋 **GROUP**: ${ctx.chat.title}
+🌟 <b>WELCOME</b> ${mention}!
 
-🆔 **YOUR ID**: ${user.id}
+📋 <b>GROUP</b>: ${ctx.chat.title}
 
-👤 **USERNAME**: ${username}
+🆔 <b>YOUR ID</b>: <code>${user.id}</code>
+
+👤 <b>USERNAME</b>: ${username}
 
 🔥 Hope you find good vibes,
 new friends and lots of fun here!
@@ -29,37 +39,47 @@ new friends and lots of fun here!
 
       await ctx.reply(
         text,
-        Markup.inlineKeyboard([
-          [
-            Markup.button.url(
-              "➕ **ADD ME IN YOUR GROUP**",
-              `https://t.me/${ctx.botInfo.username}?startgroup=true`
-            )
-          ]
-        ])
+        {
+          parse_mode: "HTML",
+          ...Markup.inlineKeyboard([
+            [
+              Markup.button.url(
+                "➕ ADD ME IN YOUR GROUP",
+                `https://t.me/${ctx.botInfo.username}?startgroup=true`
+              )
+            ]
+          ])
+        }
       );
+
     }
 
   });
 
-  // LEFT MESSAGE
+  // MEMBER LEFT
   bot.on("left_chat_member", async (ctx) => {
 
     const user = ctx.message.left_chat_member;
 
-    const username =
-      user.username
-        ? "@" + user.username
-        : "NOT SET";
+    const fullName = [
+      user.first_name,
+      user.last_name
+    ]
+      .filter(Boolean)
+      .join(" ");
+
+    const username = user.username
+      ? "@" + user.username
+      : "NOT SET";
 
     const text = `
-😢 **GOODBYE** @${user.first_name}
+😢 <b>GOODBYE</b> ${fullName}
 
-📋 **GROUP**: ${ctx.chat.title}
+📋 <b>GROUP</b>: ${ctx.chat.title}
 
-🆔 **USER ID**: ${user.id}
+🆔 <b>USER ID</b>: <code>${user.id}</code>
 
-👤 **USERNAME**:@ ${username}
+👤 <b>USERNAME</b>: ${username}
 
 💔 User left the group.
 
@@ -68,14 +88,17 @@ We will miss you!
 
     await ctx.reply(
       text,
-      Markup.inlineKeyboard([
-        [
-          Markup.button.url(
-            "➕ ADD ME IN YOUR GROUP",
-            `https://t.me/${ctx.botInfo.username}?startgroup=true`
-          )
-        ]
-      ])
+      {
+        parse_mode: "HTML",
+        ...Markup.inlineKeyboard([
+          [
+            Markup.button.url(
+              "➕ ADD ME IN YOUR GROUP",
+              `https://t.me/${ctx.botInfo.username}?startgroup=true`
+            )
+          ]
+        ])
+      }
     );
 
   });
