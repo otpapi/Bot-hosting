d273b59a52b490b6b9d3a155a1a8c55c859ee671
otpapi <otp.sender.api@gmail.com>
2026-05-31 21:43:13 +0530

    Update welcome.js

--- events/welcome.js
+++ events/welcome.js
@@ -1,32 +1,81 @@
+const { Markup } = require("telegraf");
+
 module.exports = (bot) => {
 
-  // Welcome
+  // JOIN MESSAGE
   bot.on("new_chat_members", async (ctx) => {
 
     const members = ctx.message.new_chat_members;
 
-    for (let user of members) {
+    for (const user of members) {
+
+      const username =
+        user.username
+          ? "@" + user.username
+          : "NOT SET";
+
+      const text = `
+🌟 WELCOME ${user.first_name}!
+
+📋 GROUP: ${ctx.chat.title}
+
+🆔 YOUR ID: ${user.id}
+
+👤 USERNAME: ${username}
+
+🔥 Hope you find good vibes,
+new friends and lots of fun here!
+`;
 
       await ctx.reply(
-        `🎉 Welcome ${user.first_name}
-
-👋 ${ctx.chat.title} me swagat hai.
-
-🔥 Enjoy your stay!`
+        text,
+        Markup.inlineKeyboard([
+          [
+            Markup.button.url(
+              "➕ ADD ME IN YOUR GROUP",
+              `https://t.me/${ctx.botInfo.username}?startgroup=true`
+            )
+          ]
+        ])
       );
-
     }
 
   });
 
-  // Goodbye
+  // LEFT MESSAGE
   bot.on("left_chat_member", async (ctx) => {
 
-    const user =
-      ctx.message.left_chat_member;
+    const user = ctx.message.left_chat_member;
+
+    const username =
+      user.username
+        ? "@" + user.username
+        : "NOT SET";
+
+    const text = `
+😢 GOODBYE ${user.first_name}
+
+📋 GROUP: ${ctx.chat.title}
+
+🆔 USER ID: ${user.id}
+
+👤 USERNAME: ${username}
+
+💔 User left the group.
+
+We will miss you!
+`;
 
     await ctx.reply(
-      `😢 ${user.first_name} left the group.`
+      text,
+      Markup.inlineKeyboard([
+        [
+          Markup.button.url(
+            "➕ ADD ME IN YOUR GROUP",
+            `https://t.me/${ctx.botInfo.username}?startgroup=true`
+          )
+        ]
+      ])
     );
 
   });
